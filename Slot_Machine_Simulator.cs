a792e1e51ac1a591a3bafaa1a5dd13176fe25e5a
Kooreis <kooreis.official@gmail.com>
2023-02-03 03:21:49 -0600

    Initialization of the program, including the creation of the random object, the slots array, the symbols array, and the balance and bet variables.

--- Slot_Machine_Simulator.cs
+++ Slot_Machine_Simulator.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -10,43 +9,5 @@ class Program
         string[] symbols = { "Cherry", "Lemon", "Orange", "Plum", "Bell", "Bar", "Seven" };
         int balance = 1000;
         int bet;
-
-        Console.WriteLine("Welcome to the Slot Machine Simulator!");
-        Console.WriteLine("You start with a balance of $1000.");
-
-        while (balance > 0)
-        {
-            Console.WriteLine("\nPlease enter your bet:");
-            bet = Convert.ToInt32(Console.ReadLine());
-
-            if (bet > balance)
-            {
-                Console.WriteLine("You cannot bet more than your current balance.");
-                continue;
-            }
-
-            balance -= bet;
-
-            for (int i = 0; i < 3; i++)
-            {
-                slots[i] = symbols[random.Next(symbols.Length)];
-                Console.Write(slots[i] + " ");
-            }
-
-            if (slots[0] == slots[1] && slots[1] == slots[2])
-            {
-                Console.WriteLine("\nCongratulations! You won $" + bet * 10 + "!");
-                balance += bet * 10;
-            }
-            else
-            {
-                Console.WriteLine("\nSorry, you lost this round.");
-            }
-
-            Console.WriteLine("Your current balance is $" + balance + ".");
-        }
-
-        Console.WriteLine("You ran out of money. Game over.");
     }
 }
-```
