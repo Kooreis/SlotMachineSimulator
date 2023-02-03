a66def9a6cd9d7385031754b515157ff143d8fcc
Kooreis <kooreis.official@gmail.com>
2023-02-03 03:21:49 -0600

    Imports necessary libraries and initializes the SlotMachineSimulator class. Sets up the main method, defines the slot options, and initializes the player's balance and bet amount.

--- Slot_Machine_Simulator.java
+++ Slot_Machine_Simulator.java
@@ -1,4 +1,3 @@
-```java
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,39 +8,3 @@ public class SlotMachineSimulator {
         String[] slots = {"Cherry", "Lemon", "Orange", "Plum", "Bell", "Bar", "Seven"};
         int balance = 1000;
         int bet;
-
-        while (true) {
-            System.out.println("Balance: $" + balance);
-            System.out.print("Enter bet (0 to quit): ");
-            bet = scanner.nextInt();
-
-            if (bet == 0) {
-                break;
-            }
-
-            if (bet > balance) {
-                System.out.println("You cannot bet more than your balance.");
-                continue;
-            }
-
-            balance -= bet;
-
-            String slot1 = slots[random.nextInt(slots.length)];
-            String slot2 = slots[random.nextInt(slots.length)];
-            String slot3 = slots[random.nextInt(slots.length)];
-
-            System.out.println(slot1 + " " + slot2 + " " + slot3);
-
-            if (slot1.equals(slot2) && slot2.equals(slot3)) {
-                int winnings = bet * 10;
-                System.out.println("You won $" + winnings + "!");
-                balance += winnings;
-            } else {
-                System.out.println("You lost.");
-            }
-        }
-
-        System.out.println("You leave with $" + balance + ".");
-    }
-}
-```
