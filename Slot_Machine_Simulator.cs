e8579a38168e876ef77ffe2e74277abc0cb2882f
Kooreis <kooreis.official@gmail.com>
2023-02-04 04:21:49 -0600

    Introduction messages to the user, welcoming them to the simulator and informing them of their starting balance.

--- Slot_Machine_Simulator.cs
+++ Slot_Machine_Simulator.cs
@@ -1,13 +1,2 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Random random = new Random();
-        string[] slots = new string[3];
-        string[] symbols = { "Cherry", "Lemon", "Orange", "Plum", "Bell", "Bar", "Seven" };
-        int balance = 1000;
-        int bet;
-    }
-}
+Console.WriteLine("Welcome to the Slot Machine Simulator!");
+Console.WriteLine("You start with a balance of $1000.");
